897e126e403b5d5280fa4b48eca00375fd22b353
sumancoding <progress.suman@gmail.com>
2022-04-19 23:26:15 +0300

    custom mui styles

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Stack, Button, styled } from "@mui/material";
+import ArchiveIcon from "@mui/icons-material/Archive";
 
-function App() {
+const App = () => {
+  const BlueButton = styled(Button)(({ theme }) => ({
+    backgroundColor: "skyblue",
+    color: "white",
+    "&:hover": { backgroundColor: "skyblue" },
+  }));
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Welcome Bro!!</h1>
+      <Stack spacing={2} direction="row">
+        <Button variant="contained" color="secondary">
+          Text
+        </Button>
+        <BlueButton startIcon={<ArchiveIcon />}>Conatined with Icon</BlueButton>
+        <BlueButton>Styled Button</BlueButton>
+      </Stack>
     </div>
   );
-}
+};
 
 export default App;
---
+++ src/theme.js
@@ -0,0 +1,16 @@
+import { createTheme } from "@mui/material/styles";
+
+export const theme = createTheme({
+  palette: {
+    primary: {
+      main: "#1760a5",
+      light: "skyblue",
+    },
+    secondary: {
+      main: "#12c630",
+    },
+    otherColor: {
+      main: "#999",
+    },
+  },
+});
