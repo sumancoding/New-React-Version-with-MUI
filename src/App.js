5e00d41aeea77af531f1ddfcebe737b24c48a3f9
sumancoding <progress.suman@gmail.com>
2022-04-20 00:05:10 +0300

    responsive mui

--- src/App.js
+++ src/App.js
@@ -1,25 +1,46 @@
 import React from "react";
-import { Stack, Button, styled } from "@mui/material";
-import ArchiveIcon from "@mui/icons-material/Archive";
+import Feed from "./components/Feed";
+import NavBar from "./components/NavBar";
+import RightBar from "./components/RightBar";
+import SideBar from "./components/SideBar";
+import { Box, Stack } from "@mui/material";
 
 const App = () => {
-  const BlueButton = styled(Button)(({ theme }) => ({
-    backgroundColor: "skyblue",
-    color: "white",
-    "&:hover": { backgroundColor: "skyblue" },
-  }));
   return (
-    <div>
-      <h1>Welcome Bro!!</h1>
-      <Stack spacing={2} direction="row">
-        <Button variant="contained" color="secondary">
-          Text
-        </Button>
-        <BlueButton startIcon={<ArchiveIcon />}>Conatined with Icon</BlueButton>
-        <BlueButton>Styled Button</BlueButton>
+    <Box>
+      <Stack direction="row" spacing={2} justifyContent="space-between">
+        <SideBar />
+        <Feed />
+        <RightBar />
       </Stack>
-    </div>
+    </Box>
   );
 };
 
 export default App;
+
+// import React from "react";
+// import { Stack, Button, styled } from "@mui/material";
+// import ArchiveIcon from "@mui/icons-material/Archive";
+
+// const App = () => {
+//   const BlueButton = styled(Button)(({ theme }) => ({
+//     backgroundColor: "skyblue",
+//     color: "white",
+//     "&:hover": { backgroundColor: "skyblue" },
+//   }));
+//   return (
+//     <div>
+//       <h1>Welcome Bro!!</h1>
+//       <Stack spacing={2} direction="row">
+//         <Button variant="contained" color="secondary">
+//           Text
+//         </Button>
+//         <BlueButton startIcon={<ArchiveIcon />}>Conatined with Icon</BlueButton>
+//         <BlueButton>Styled Button</BlueButton>
+//       </Stack>
+//     </div>
+//   );
+// };
+
+// export default App;
